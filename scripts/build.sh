426ffe6689c3af25b920afbe9fa90f0b9dff0e22
Jixun <jixun.moe@gmail.com>
2020-09-13 19:34:51 +0100

    add compile target for armv6 and arm64v8

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,11 +11,17 @@ build() {
   export GOOS="$1"
   export GOARCH="$2"
   EXT=""
+  ARM_VER=""
   if [ "${GOOS}" = "windows" ]; then
     EXT=".exe"
+  elif [ "${GOARCH}" = "arm" ]; then
+    ARM_VER="v${GOARM}"
+  elif [ "${GOARCH}" = "arm64" ]; then
+    # arm64 = arm v8
+    ARM_VER="v8"
   fi
 
-  NAME="backup-cli-${GOOS}-${GOARCH}"
+  NAME="backup-cli-${GOOS}-${GOARCH}${ARM_VER}"
   OUT_PATH="${BINDIR}/${NAME}"
   echo "Building ${NAME}..."
   go build -trimpath -ldflags '-w -s' -o "${OUT_PATH}${EXT}" ./cmd/backup-cli
@@ -33,5 +39,7 @@ build windows amd64
 build linux amd64
 
 # arm: arm v6 (Raspberry Pi 1/Zero/Zero W/CM)
+# arm64: rpi4 with some
 # other arm arch does not seem to be included by default?
-build linux arm
+GOARM=6 build linux arm
+build linux arm64
