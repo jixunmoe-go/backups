30d4602b6c7732b238f6d997031aeec7f4b0be64
Jixun <jixun.moe@gmail.com>
2020-09-15 20:39:48 +0100

    fix memset api call

--- utils/memory/memset.go
+++ utils/memory/memset.go
@@ -1,6 +1,7 @@
 package memory
 
 // #include <string.h>
+// void ClearMem(void* ptr, int size) {memset(ptr, 0, (size_t)size);}
 import "C"
 import "unsafe"
 
@@ -9,7 +10,7 @@ const zeroBufSize = 4 * 1024
 var zeros = make([]byte, zeroBufSize)
 
 func setZeroC(ptr []byte) {
-	C.memset(unsafe.Pointer(&ptr[0]), C.int(0), C.ulonglong(len(ptr)))
+	C.ClearMem(unsafe.Pointer(&ptr[0]), C.int(len(ptr)))
 }
 
 // SetZero is the fastest implementation to zero out a given slice.
